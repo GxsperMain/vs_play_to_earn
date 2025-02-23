05d0ab6b74b20f1d174dfcfde4d3965ac54486f2
GxsperMain <gxspmain@proton.me>
2025-02-22 22:36:15 -0300

    1.0.4²

--- PlayToEarn/Configuration.cs
+++ PlayToEarn/Configuration.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 using Newtonsoft.Json;
 using Vintagestory.API.Common;
